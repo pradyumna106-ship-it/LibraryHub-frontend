d1fe55d37115e6b6eba277a277a63e7c6a4e0325
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-04-14 16:02:00 +0530

    emplemented .env

--- src/api/base.js
+++ src/api/base.js
@@ -1,22 +1,6 @@
 import axios from "axios";
 
-// ✅ Member Login FUNCTION
-export const memberLogin = (data) => {
-  return API.post("/member/login", data);
-};
-
-// ✅ Admin Login FUNCTION
-export const adminLogin = (data) => {
-  return API.post("/admin/login", data);
-};
-
-// export const memberSignUp = (data) => {
-//   return axios.post("http://localhost:4000/api/v1/member/login", data, {
-//     "Content-Type": "application/json"
-//   });
-// };
-
-const API_BASE_URL =  "https://vercel.com/j-pradyumnas-projects/library-hub-backend/6gyogLm29oeeoD2uYRNkQtgzL64E/api/v1";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const API = axios.create({
   baseURL: API_BASE_URL,
@@ -25,6 +9,10 @@ const API = axios.create({
   },
 });
 
+// ✅ Member Login
+export const memberLogin = (data) => API.post("/member/login", data);
 
+// ✅ Admin Login
+export const adminLogin = (data) => API.post("/admin/login", data);
 
 export default API;
