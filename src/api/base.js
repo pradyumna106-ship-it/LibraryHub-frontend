1a5df52cc2c113ce5388fd3c22f10404ee602419
pr4dyumn4 <pradyumnajekumar@gmail.com>
2026-04-15 17:59:03 +0530

    updated base.js

--- src/api/base.js
+++ src/api/base.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const API = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
@@ -11,12 +11,12 @@ const API = axios.create({
 
 // ✅ Member Login
 export const memberLogin = (data) => axios.post(
-  "https://library-hub-backend-nine.vercel.app/api/v1/member/login",
+  `${API_BASE_URL}/api/v1/member/login`,
   data
 );
 // ✅ Admin Login
 export const adminLogin = (data) => axios.post(
-  "https://library-hub-backend-nine.vercel.app/api/v1/admin/login",
+  `${API_BASE_URL}/api/v1/admin/login`,
   data
 );
 export default API;
